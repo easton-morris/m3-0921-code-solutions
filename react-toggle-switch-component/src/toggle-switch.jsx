5ef4f0b6b26c5d4bf5324bfc8ecb4fd12096d1a7
easton-morris <eastonmorris9@gmail.com>
2021-10-14 21:12:27 -0700

    simplified state

--- react-toggle-switch-component/src/toggle-switch.jsx
+++ react-toggle-switch-component/src/toggle-switch.jsx
@@ -4,21 +4,21 @@ class ToggleSwitch extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { button: true, switch: true, label: true };
+    this.state = { toggled: true };
     this.clickHandler = this.clickHandler.bind(this);
   }
 
   clickHandler() {
     this.setState(prevState => (
-      { button: !prevState.button, switch: !prevState.switch, label: !prevState.label }
+      { toggled: !prevState.status }
     ));
   }
 
   render() {
     return <div>
-      <div onClick={this.clickHandler} className={ `switch ${this.state.switch ? 'switchedOn' : 'switchedOff'}` }></div>
-      <button onClick={this.clickHandler} className={this.state.button ? 'on' : 'off'}></button>
-      <span className="label">{this.state.label ? 'ON' : 'OFF'}</span>
+      <div onClick={this.clickHandler} className={ `switch ${this.state.toggled ? 'switchedOn' : 'switchedOff'}` }></div>
+      <button onClick={this.clickHandler} className={this.state.toggled ? 'on' : 'off'}></button>
+      <span className="label">{this.state.toggled ? 'ON' : 'OFF'}</span>
     </div>;
   }
 }
