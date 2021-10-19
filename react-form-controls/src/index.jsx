ab6d7a521b1be2d5cf72b526e096af10f00088ef
Easton Morris <eastonmorris9@gmail.com>
2021-10-18 19:57:13 -0700

    react-form-controls

---
+++ react-form-controls/src/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class NewsletterForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange() {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit() {
+    console.log('Email:', this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (<form id="newsletter" onSubmit={this.handleSubmit}>
+      <label htmlFor="email">Email</label>
+      <input id="email" type="text" value={this.state.value} onChange={this.handleChange}/>
+      <button>
+        Submit
+      </button>
+    </form>
+    );
+  }
+}
+
+ReactDOM.render(
+  <NewsletterForm />,
+  document.getElementById('root')
+);
