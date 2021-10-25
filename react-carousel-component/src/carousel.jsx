03ca94af246bafd56b5ca0e62ad5539490992b77
Easton Morris <eastonmorris9@gmail.com>
2021-10-25 00:03:13 -0700

    new builders and handlers set up

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -4,26 +4,69 @@ class Carousel extends React.Component {
   constructor(props) {
     super(props);
 
-    this.props = props;
-    this.state = { active: true, caroNum: 0 };
+    this.state = { caroNum: 0, active: true };
+    this.arrowClickHandler = this.arrowClickHandler.bind(this);
+    this.iconClickHandler = this.iconClickHandler.bind(this);
   }
 
-  clickHandler(event) {
+  arrowClickHandler(event) {
     if (event.target.className === 'fas fa-arrow-right' && this.state.caroNum !== (this.props.pics.length - 1)) {
-      this.setState({ caroNum: this.state.caroNum + 1, active: false });
+      this.setState({ caroNum: (this.state.caroNum + 1), active: false });
     } else if (this.state.caroNum === this.props.pics.length) {
       this.setState({ caroNum: 0, active: false });
     } else if (event.target.className === 'fas fa-arrow-left' && this.state.caroNum !== 0) {
-      this.setState({ caroNum: this.state.caroNum - 1, active: false });
+      this.setState({ caroNum: (this.state.caroNum - 1), active: false });
     } else if (this.state.caroNum === 0) {
-      this.setState({ caroNum: this.props.pics.length, active: false });
+      this.setState({ caroNum: (this.props.pics.length - 1), active: false });
+    }
+  }
+
+  iconClickHandler(event) {
+    if (event.target.className === 'far fa-dot-circle') {
+      event.target.className = 'fas fa-dot-circle';
+    }
+
+    const iconList = document.getElementsByName('i');
+
+    for (let ii = 0; ii < iconList.length; ii++) {
+      iconList[ii].className = 'far fa-dot-circle';
+    }
+
+    this.setState({ caroNum: event.target.iconId, active: false });
+
+  }
+
+  IconItem(props) {
+
+  }
+
+  IconList(props) {
+
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.active === false) {
+      window.setInterval();
     }
   }
 
   CaroPic(props) {
     return (
-      <div className="caroPicArea">
        <img className="caroPic" src={props.picUrl} alt={props.charName} />
+    );
+  }
+
+  render() {
+    return (
+      <div>
+      <div className="caroArea">
+        <i className="fas fa-arrow-left"></i>
+        <this.CaroPic picUrl={this.props[this.state.caroNum].URL} charName={this.props[this.state.caroNum].char} />
+        <i className="fas fa-arrow-right"></i>
+      </div>
+      <div>
+        <this.IconList />
+      </div>
       </div>
     );
   }
