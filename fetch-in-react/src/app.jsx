fd6636cd2834649fe3249564e094172e5d8c3582
Easton Morris <eastonmorris9@gmail.com>
2021-10-28 20:02:26 -0700

    fetch-in-react

---
+++ fetch-in-react/src/app.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import UserList from './user-list';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      isLoading: true
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => {
+        if (!res.ok) {
+          throw new Error('woops');
+        } else {
+          return res.json();
+        }
+      })
+      .then(data => {
+        return this.setState({
+          users: data, isLoading: false
+        }
+        );
+      }
+      )
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    return this.state.isLoading
+      ? <p>Loading...</p>
+      : <UserList users={this.state.users} />;
+  }
+}
+
+export default App;
