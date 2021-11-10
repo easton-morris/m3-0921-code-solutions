36074c6441dfb75c160c4e529e22dbed43939415
Easton Morris <eastonmorris9@gmail.com>
2021-11-09 20:54:15 -0800

    adding the template literal and componentdidmount mething

--- react-hash-routing/client/app.jsx
+++ react-hash-routing/client/app.jsx
@@ -14,6 +14,11 @@ export default class App extends React.Component {
   }
 
   componentDidMount() {
+    window.addEventListener('hashchange', () => {
+      this.setState(prevState => (
+        { route: parseRoute(window.location.hash) }
+      ));
+    });
     /**
      * Listen for hash change events on the window object
      * Each time the window.location.hash changes, parse
